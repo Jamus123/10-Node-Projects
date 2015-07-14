faf46963c170f2c4cd9f9cb8ce13f6981617e87b
Jamus123 <jactenors@gmail.com>
2015-07-14 15:50:28 -0700

    Simple file server fin

--- Project1/server.js
+++ Project1/server.js
@@ -1,6 +1,49 @@
 var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+var url = require('url');
+var path = require('path');
+var fs = require('fs');
+
+//Array
+var mimeTypes = {
+  "html" : "text/html",
+  "jpeg" : "image/jpeg",
+  "JPG" : "image/jpg",
+  "png" : "image/png",
+  "js" : "text/javascript",
+  "css" : "text/css"
+};
+
+//create server
+http.createServer(function(req, res){
+  var uri = url.parse(req.url).pathname;
+  var fileName = path.join(process.cwd(), unescape(uri));
+  console.log("Loading " + uri);
+  var stats;
+
+  try{
+    stats = fs.lstatSync(fileName);
+  } catch(e) {
+    res.writeHead(404, {'Content-type' : "text/plain"});
+    res.write('404 Not Found');
+    res.end();
+    return;
+  }
+
+  //Check if file/directory
+  if(stats.isFile()){
+    var mimeType = mimeTypes[path.extname(fileName).split('.').reverse()[0]];
+    res.writeHead(200, {'Content-type' : mimeType});
+
+    var fileStream = fs.createReadStream(fileName);
+    fileStream.pipe(res);
+  }else if (stats.isDirectory()) {
+    res.writeHead(302,{
+      'location' : 'index.html'
+    });
+    res.end();
+  }else{
+    res.writeHead(500,{'Content-type' : 'text/plain'});
+    res.write('500 internal error');
+    res.end();
+  }
+}).listen(3000);
